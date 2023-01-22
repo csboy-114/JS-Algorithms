fbbaa78fe7992536ea910c82177b069e0a93c446
csboy-114 <1060437021@qq.com>
2023-01-22 17:00:38 +0800

    哈希表开放寻址，冲突key值占满空位。

---
+++ hash-table-opensearch.js
@@ -0,0 +1,68 @@
+class HashTable{
+  constructor(params) {
+    this.size = 100;
+    this.buckets =Array(100).fill(null)
+  }
+  hash(key){
+    let hash = 0
+    for(const char of key){
+      hash+=char.charCodeAt(0)
+    }
+    return hash % this.size
+  }
+  set(key,value){
+    let keyHash = this.hash(key)
+    if(this.buckets[keyHash]===null || this.buckets[keyHash].key === key){
+      this.buckets[keyHash] = {key:key,val:value}
+    }else{
+      while (this.buckets[keyHash]!==null) {
+        keyHash++
+      }
+      this.buckets[keyHash] = {key:key,val:value}
+    }
+  }
+  get(key){
+    const keyHash = this.hash(key)
+    for(let i=keyHash;i<this.buckets.length;i++){
+      if(!this.buckets[i]){
+         continue;
+      }
+      if(this.buckets[i].key === key){
+        return this.buckets[i].val
+      }
+    }
+    return undefined
+  }
+  showInfo(){
+    for(const key in this.buckets){
+      if(this.buckets[key] !== null){
+        console.log(key,this.buckets[key]);
+      }
+    }
+  }
+}
+
+const table1 = new HashTable()
+for(const char of 'academind'){
+  table1.set(char,1)
+}
+for(const char of 'hello'){
+  table1.set(char,1)
+}
+for(const char of 'does this work'){
+  table1.set(char,1)
+}
+table1.showInfo()
+
+// const word = 'hello'
+// function findFirstRep(str){
+//   const table = new HashTable()
+//   for(const char of str){
+//     if(table.get(char)){
+//       return char
+//     }
+//     table.set(char,1)
+//   }
+// }
+
+// console.log(findFirstRep(word));
