eac498b0621a969f7d494bf11c515f47721532ff
csboy-114 <1060437021@qq.com>
2023-01-23 12:04:00 +0800

    删除树节点。

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 class Node{
   constructor(value,parentNode=null) {
-    this.parent = parentNode
-    this.value = value
     this.children = []
+    this.value = value
+    this.parent = parentNode
   }
-  addNode(val){
-    const node = new Node(val,this)
-    this.children.push(node)
-    return {node:node,index:this.children.length-1}
+  addNode(value){
+    const segments = value.split('/')
+    if(segments.length===0){
+      return
+    }
+    if(segments.length===1){
+      const node = new Node(segments[0],this)
+      this.children.push(node)
+      return {node:node,index:this.children.length-1}
+    }
+    const existingChildNode = this.children.find(child=>child.value === segments[0])
+    if(existingChildNode){
+      existingChildNode.addNode(segments.slice(1).join('/'))
+    }else{
+     const node = new Node(segments[0],this)
+     node.addNode(segments.slice(1).join('/'))
+     this.children.push(node)
+     return {node:node,index:this.children.length-1}
+    }
   }
-  removeNode(index){
-    this.children.splice(index,1)
+  removeNode(value){
+    const segments = value.split('/')
+    if(segments.length===0){
+      return
+    }
+    if(segments.length===1){
+      const existingNodeIndex = this.children.findIndex((child)=>child.value === segments[0])
+      if(existingNodeIndex<0){
+        throw new Error('Could not find Matching Value')
+      }
+      this.children.splice(existingNodeIndex,1)
+    }
+    if(segments.length>1){
+      const existingChildNode = this.children.find(child=>child.value === segments[0])
+      if(!existingChildNode){
+        throw new Error('Could not find matching path! Path segment：'+segments[0])
+      }
+      existingChildNode.removeNode(segments.slice(1).join('/'))
+    }
   }
 }
 
@@ -19,14 +51,19 @@ class Tree{
   constructor(rootValue) {
     this.root = new Node(rootValue)
   }
+  add(path){
+    this.root.addNode(path)
+  }
+  remove(path){
+    this.root.removeNode(path)
+  }
 }
 
 const filesystem = new Tree('/')
 
-const documentsData = filesystem.root.addNode('/document')
-const gamesNodeData =  filesystem.root.addNode('/games')
-
-documentsData.node.addNode('results.txt')
-gamesNodeData.node.addNode('cod.exe')
-
+filesystem.add('documents/personal/tax.docx')
+filesystem.add('games/cod.exe')
+filesystem.add('games/cod2.exe')
+filesystem.remove('games/cod.exe') 
+filesystem.remove('games/cod3.exe') 
 console.log(filesystem);
