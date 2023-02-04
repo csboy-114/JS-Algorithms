660dd714887154848c3f364fc124ef958eefc073
csboy-114 <1060437021@qq.com>
2023-02-04 19:48:04 +0800

    二分查找递归形式

---
+++ js-algorithm/bineary-search.js
@@ -0,0 +1,32 @@
+function findElement(arr,target){
+  debugger
+  let half = ~~(arr.length-1/2)
+  while(target!== arr[half]){
+    if(target<arr[half]){
+      half /=2 
+    }else if(target>arr[half]){
+      half = ~~((half + arr.length-1)/2)
+    }else {
+      target = arr[half]
+    }
+  }
+  return target
+}
+// 递归形式
+function findElement1(arr,target){
+  debugger
+  let half = ~~((arr.length-1)/2)
+  
+    if(target<arr[half]){
+      half /=2 
+      findElement1(arr.slice(0,half),target)
+    }else if(target>arr[half]){
+      half = ~~((half + arr.length-1)/2)
+      findElement1(arr.slice(half),target)
+    }else {
+      target = arr[half]
+    }
+  return target
+}
+const arr = [1,5,9,13,99,100]
+console.log(findElement(arr,99));
